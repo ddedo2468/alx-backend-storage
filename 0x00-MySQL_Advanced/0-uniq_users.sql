97f3895a3aaa1025ff87b6968f2a5c903826b7be
Abdullah <abdallahmosbah25@gmail.com>
2024-04-24 18:51:53 +0200

    first task

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- create users table;
+-- shouldn't fail on any db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
